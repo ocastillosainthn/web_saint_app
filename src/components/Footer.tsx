4b59418482372c09c6878de00f39be98a181f148
ocastillosainthn <ocastillo@sainthn.com>
2026-02-03 16:17:59 -0600

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Facebook, Instagram, Mail, Phone, MapPin, Shield, Smartphone, MessageCircle } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
